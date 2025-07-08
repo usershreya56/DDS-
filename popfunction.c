bddd60054f430c4ceaa23cc152821223bc1b99bc
usershreya56 <shreyapatel6510@gmail.com>
2025-07-08 09:03:07 +0530

    Updatepopfunction.c

--- popfunction.c
+++ popfunction.c
@@ -3,6 +3,16 @@ using namespace std;
 #define size 5
 int stack[size];
 int top=-1;
+void push(int num){
+    if(top<size-1){
+        top++;
+        stack[top]=num;
+        cout<<num<<"inserted"<<endl;
+    }
+    else{
+        cout<<"stack is overflow"<<endl;
+    }
+}
 void pop(){
     if(top==-1){
         cout<<"stack is underflow"<<endl;
